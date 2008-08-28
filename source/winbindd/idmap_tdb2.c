samba-team/samba

Remove "idmap alloc config : range" parameter
This was overwritten by "idmap uid/gid" anyway. These are now the range
parameters for the alloc backend.
(This used to be commit d563a7b80dc3e759069db2cd54d596a1b8c55191)

source3/winbindd/idmap_tdb2.c
@@ -128,62 +128,46 @@ static NTSTATUS idmap_tdb2_alloc_load(void)
 	uid_t high_uid = 0;
 	gid_t low_gid = 0;
 	gid_t high_gid = 0;
-
-	/* load ranges */
-	idmap_tdb2_state.low_uid = 0;
-	idmap_tdb2_state.high_uid = 0;
-	idmap_tdb2_state.low_gid = 0;
-	idmap_tdb2_state.high_gid = 0;
+	uint32 low_id, high_id;
 
 	/* see if a idmap script is configured */
-	idmap_tdb2_state.idmap_script = lp_parm_const_string(-1, "idmap", "script", NULL);
+	idmap_tdb2_state.idmap_script = lp_parm_const_string(-1, "idmap",
+							     "script", NULL);
 
 	if (idmap_tdb2_state.idmap_script) {
-		DEBUG(1, ("using idmap script '%s'\n", idmap_tdb2_state.idmap_script));
+		DEBUG(1, ("using idmap script '%s'\n",
+			  idmap_tdb2_state.idmap_script));
 	}
 
-	range = lp_parm_const_string(-1, "idmap alloc config", "range", NULL);
-	if (range && range[0]) {
-		unsigned low_id, high_id;
-		if (sscanf(range, "%u - %u", &low_id, &high_id) == 2) {
-			if (low_id < high_id) {
-				idmap_tdb2_state.low_gid = idmap_tdb2_state.low_uid = low_id;
-				idmap_tdb2_state.high_gid = idmap_tdb2_state.high_uid = high_id;
-			} else {
-				DEBUG(1, ("ERROR: invalid idmap alloc range [%s]", range));
-			}
-		} else {
-			DEBUG(1, ("ERROR: invalid syntax for idmap alloc config:range [%s]", range));
-		}
-	}
+	/* load ranges */
 
 	/* Create high water marks for group and user id */
-	if (lp_idmap_uid(&low_uid, &high_uid)) {
-		idmap_tdb2_state.low_uid = low_uid;
-		idmap_tdb2_state.high_uid = high_uid;
+	if (!lp_idmap_uid(&low_uid, &high_uid)
+	    || !lp_idmap_gid(&low_gid, &high_gid)) {
+		DEBUG(1, ("idmap uid or idmap gid missing\n"));
+		return NT_STATUS_UNSUCCESSFUL;
 	}
 
-	if (lp_idmap_gid(&low_gid, &high_gid)) {
-		idmap_tdb2_state.low_gid = low_gid;
-		idmap_tdb2_state.high_gid = high_gid;
-	}
+	idmap_tdb2_state.low_uid = low_uid;
+	idmap_tdb2_state.high_uid = high_uid;
+	idmap_tdb2_state.low_gid = low_gid;
+	idmap_tdb2_state.high_gid = high_gid;
 
 	if (idmap_tdb2_state.high_uid <= idmap_tdb2_state.low_uid) {
 		DEBUG(1, ("idmap uid range missing or invalid\n"));
 		DEBUGADD(1, ("idmap will be unable to map foreign SIDs\n"));
 		return NT_STATUS_UNSUCCESSFUL;
-	} else {
-		uint32 low_id;
+	}
 
-		if (((low_id = dbwrap_fetch_int32(idmap_tdb2_perm,
-						  HWM_USER)) == -1) ||
-		    (low_id < idmap_tdb2_state.low_uid)) {
-			if (dbwrap_store_int32(
-				    idmap_tdb2_perm, HWM_USER,
-				    idmap_tdb2_state.low_uid) == -1) {
-				DEBUG(0, ("Unable to initialise user hwm in idmap database\n"));
-				return NT_STATUS_INTERNAL_DB_ERROR;
-			}
+	if (((low_id = dbwrap_fetch_int32(idmap_tdb2_perm,
+					  HWM_USER)) == -1) ||
+	    (low_id < idmap_tdb2_state.low_uid)) {
+		if (dbwrap_store_int32(
+			    idmap_tdb2_perm, HWM_USER,
+			    idmap_tdb2_state.low_uid) == -1) {
+			DEBUG(0, ("Unable to initialise user hwm in idmap "
+				  "database\n"));
+			return NT_STATUS_INTERNAL_DB_ERROR;
 		}
 	}
 
@@ -191,18 +175,17 @@ static NTSTATUS idmap_tdb2_alloc_load(void)
 		DEBUG(1, ("idmap gid range missing or invalid\n"));
 		DEBUGADD(1, ("idmap will be unable to map foreign SIDs\n"));
 		return NT_STATUS_UNSUCCESSFUL;
-	} else {
-		uint32 low_id;
+	}
 
-		if (((low_id = dbwrap_fetch_int32(idmap_tdb2_perm,
-						  HWM_GROUP)) == -1) ||
-		    (low_id < idmap_tdb2_state.low_gid)) {
-			if (dbwrap_store_int32(
-				    idmap_tdb2_perm, HWM_GROUP,
-				    idmap_tdb2_state.low_gid) == -1) {
-				DEBUG(0, ("Unable to initialise group hwm in idmap database\n"));
-				return NT_STATUS_INTERNAL_DB_ERROR;
-			}
+	if (((low_id = dbwrap_fetch_int32(idmap_tdb2_perm,
+					  HWM_GROUP)) == -1) ||
+	    (low_id < idmap_tdb2_state.low_gid)) {
+		if (dbwrap_store_int32(
+			    idmap_tdb2_perm, HWM_GROUP,
+			    idmap_tdb2_state.low_gid) == -1) {
+			DEBUG(0, ("Unable to initialise group hwm in idmap "
+				  "database\n"));
+			return NT_STATUS_INTERNAL_DB_ERROR;
 		}
 	}
 

gwr/samba-old

source/winbindd/idmap_tdb2.c
@@ -99,62 +99,46 @@ static NTSTATUS idmap_tdb2_alloc_load(void)
 	uid_t high_uid = 0;
 	gid_t low_gid = 0;
 	gid_t high_gid = 0;
-
-	/* load ranges */
-	idmap_tdb2_state.low_uid = 0;
-	idmap_tdb2_state.high_uid = 0;
-	idmap_tdb2_state.low_gid = 0;
-	idmap_tdb2_state.high_gid = 0;
+	uint32 low_id, high_id;
 
 	/* see if a idmap script is configured */
-	idmap_tdb2_state.idmap_script = lp_parm_const_string(-1, "idmap", "script", NULL);
+	idmap_tdb2_state.idmap_script = lp_parm_const_string(-1, "idmap",
+							     "script", NULL);
 
 	if (idmap_tdb2_state.idmap_script) {
-		DEBUG(1, ("using idmap script '%s'\n", idmap_tdb2_state.idmap_script));
+		DEBUG(1, ("using idmap script '%s'\n",
+			  idmap_tdb2_state.idmap_script));
 	}
 
-	range = lp_parm_const_string(-1, "idmap alloc config", "range", NULL);
-	if (range && range[0]) {
-		unsigned low_id, high_id;
-		if (sscanf(range, "%u - %u", &low_id, &high_id) == 2) {
-			if (low_id < high_id) {
-				idmap_tdb2_state.low_gid = idmap_tdb2_state.low_uid = low_id;
-				idmap_tdb2_state.high_gid = idmap_tdb2_state.high_uid = high_id;
-			} else {
-				DEBUG(1, ("ERROR: invalid idmap alloc range [%s]", range));
-			}
-		} else {
-			DEBUG(1, ("ERROR: invalid syntax for idmap alloc config:range [%s]", range));
-		}
-	}
+	/* load ranges */
 
 	/* Create high water marks for group and user id */
-	if (lp_idmap_uid(&low_uid, &high_uid)) {
-		idmap_tdb2_state.low_uid = low_uid;
-		idmap_tdb2_state.high_uid = high_uid;
+	if (!lp_idmap_uid(&low_uid, &high_uid)
+	    || !lp_idmap_gid(&low_gid, &high_gid)) {
+		DEBUG(1, ("idmap uid or idmap gid missing\n"));
+		return NT_STATUS_UNSUCCESSFUL;
 	}
 
-	if (lp_idmap_gid(&low_gid, &high_gid)) {
-		idmap_tdb2_state.low_gid = low_gid;
-		idmap_tdb2_state.high_gid = high_gid;
-	}
+	idmap_tdb2_state.low_uid = low_uid;
+	idmap_tdb2_state.high_uid = high_uid;
+	idmap_tdb2_state.low_gid = low_gid;
+	idmap_tdb2_state.high_gid = high_gid;
 
 	if (idmap_tdb2_state.high_uid <= idmap_tdb2_state.low_uid) {
 		DEBUG(1, ("idmap uid range missing or invalid\n"));
 		DEBUGADD(1, ("idmap will be unable to map foreign SIDs\n"));
 		return NT_STATUS_UNSUCCESSFUL;
-	} else {
-		uint32 low_id;
+	}
 
-		if (((low_id = dbwrap_fetch_int32(idmap_tdb2,
-						  HWM_USER)) == -1) ||
-		    (low_id < idmap_tdb2_state.low_uid)) {
-			if (!NT_STATUS_IS_OK(dbwrap_trans_store_int32(
-						     idmap_tdb2, HWM_USER,
-						     idmap_tdb2_state.low_uid))) {
-				DEBUG(0, ("Unable to initialise user hwm in idmap database\n"));
-				return NT_STATUS_INTERNAL_DB_ERROR;
-			}
+	if (((low_id = dbwrap_fetch_int32(idmap_tdb2,
+					  HWM_USER)) == -1) ||
+	    (low_id < idmap_tdb2_state.low_uid)) {
+		if (dbwrap_store_int32(
+			    idmap_tdb2, HWM_USER,
+			    idmap_tdb2_state.low_uid) == -1) {
+			DEBUG(0, ("Unable to initialise user hwm in idmap "
+				  "database\n"));
+			return NT_STATUS_INTERNAL_DB_ERROR;
 		}
 	}
 
@@ -162,18 +146,17 @@ static NTSTATUS idmap_tdb2_alloc_load(void)
 		DEBUG(1, ("idmap gid range missing or invalid\n"));
 		DEBUGADD(1, ("idmap will be unable to map foreign SIDs\n"));
 		return NT_STATUS_UNSUCCESSFUL;
-	} else {
-		uint32 low_id;
+	}
 
-		if (((low_id = dbwrap_fetch_int32(idmap_tdb2,
-						  HWM_GROUP)) == -1) ||
-		    (low_id < idmap_tdb2_state.low_gid)) {
-			if (!NT_STATUS_IS_OK(dbwrap_trans_store_int32(
-						     idmap_tdb2, HWM_GROUP,
-						     idmap_tdb2_state.low_gid))) {
-				DEBUG(0, ("Unable to initialise group hwm in idmap database\n"));
-				return NT_STATUS_INTERNAL_DB_ERROR;
-			}
+	if (((low_id = dbwrap_fetch_int32(idmap_tdb2,
+					  HWM_GROUP)) == -1) ||
+	    (low_id < idmap_tdb2_state.low_gid)) {
+		if (dbwrap_store_int32(
+			    idmap_tdb2, HWM_GROUP,
+			    idmap_tdb2_state.low_gid) == -1) {
+			DEBUG(0, ("Unable to initialise group hwm in idmap "
+				  "database\n"));
+			return NT_STATUS_INTERNAL_DB_ERROR;
 		}
 	}
 