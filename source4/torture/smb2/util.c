c3d02db639faf405bc9037aa7bb245b88130eb0b
Gordon Ross <gwr@nexenta.com>
2016-08-16 21:51:20 -0400

    NT_STATUS_EQUAL etc.

--- source4/torture/smb2/util.c
+++ source4/torture/smb2/util.c
@@ -98,7 +98,7 @@ static NTSTATUS smb2_create_complex(struct smb2_tree *tree, const char *fname,
 	}
 
 	status = smb2_create(tree, tmp_ctx, &io);
-	if (status == NT_STATUS_EAS_NOT_SUPPORTED) {
+	if (NT_STATUS_EQUAL(status, NT_STATUS_EAS_NOT_SUPPORTED)) {
 		printf("EAs not supported, creating: %s\n", fname);
 		io.in.eas.num_eas = 0;
 		status = smb2_create(tree, tmp_ctx, &io);
