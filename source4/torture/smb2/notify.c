24ee0ae05f1bc16a10d3d3280d6b7666fc51a9ab
Matt Barden <mbarden@tintri.com>
2020-09-03 17:11:34 -0400

    notify-after-close panic test

--- source4/torture/smb2/notify.c
+++ source4/torture/smb2/notify.c
@@ -2144,6 +2144,82 @@ static bool torture_smb2_notify_rmdir4(struct torture_context *torture,
 	return torture_smb2_notify_rmdir(torture, tree1, tree2, true);
 }
 
+static bool torture_smb2_notify_close_after(struct torture_context *torture,
+					    struct smb2_tree *tree1,
+					    struct smb2_tree *tree2)
+{
+	bool ret = true;
+	NTSTATUS status;
+	union smb_notify notify = {};
+	union smb_setfileinfo sfinfo = {};
+	union smb_open io = {};
+	struct smb2_handle h = {};
+	struct smb2_handle h1 = {};
+	struct smb2_request *req;
+
+	torture_comment(torture, "TESTING NOTIFY CLOSE WHILE NOTIFY\n");
+
+	smb2_deltree(tree1, BASEDIR);
+	smb2_util_rmdir(tree1, BASEDIR);
+
+	ZERO_STRUCT(io.smb2);
+	io.generic.level = RAW_OPEN_SMB2;
+	io.smb2.in.create_flags = 0;
+	io.smb2.in.desired_access = SEC_FILE_ALL;
+	io.smb2.in.create_options = NTCREATEX_OPTIONS_DIRECTORY;
+	io.smb2.in.file_attributes = FILE_ATTRIBUTE_NORMAL;
+	io.smb2.in.share_access =
+		NTCREATEX_SHARE_ACCESS_READ |
+		NTCREATEX_SHARE_ACCESS_WRITE |
+		NTCREATEX_SHARE_ACCESS_DELETE ;
+	io.smb2.in.alloc_size = 0;
+	io.smb2.in.create_disposition = NTCREATEX_DISP_OPEN_IF;
+	io.smb2.in.impersonation_level = SMB2_IMPERSONATION_ANONYMOUS;
+	io.smb2.in.security_flags = 0;
+	io.smb2.in.fname = BASEDIR;
+
+	status = smb2_create(tree1, torture, &(io.smb2));
+	CHECK_STATUS(status, NT_STATUS_OK);
+	h = io.smb2.out.file.handle;
+
+	status = smb2_create(tree2, torture, &(io.smb2));
+	CHECK_STATUS(status, NT_STATUS_OK);
+	h1 = io.smb2.out.file.handle;
+
+	ZERO_STRUCT(notify.smb2);
+	notify.smb2.level = RAW_NOTIFY_SMB2;
+	notify.smb2.in.buffer_size = 1000;
+	notify.smb2.in.completion_filter = FILE_NOTIFY_CHANGE_NAME;
+	notify.smb2.in.file.handle = h;
+	notify.smb2.in.recursive = false;
+
+	io.smb2.in.desired_access |= SEC_STD_DELETE;
+	io.smb2.in.create_disposition = NTCREATEX_DISP_OPEN;
+	req = smb2_notify_send(tree1, &(notify.smb2));
+
+	smb_msleep(100);
+	smb2_util_close(tree1, h);
+
+	smb_msleep(500);
+	io.smb2.in.fname = BASEDIR "\\notifydir";
+	status = smb2_create(tree2, torture, &(io.smb2));
+	smb2_util_close(tree2, io.smb2.out.file.handle);
+	//status = smb2_notify_recv(req, torture, &(notify.smb2));
+	smb_msleep(30000);
+done:
+
+	smb2_util_close(tree1, h);
+	smb2_util_close(tree1, h1);
+	smb2_deltree(tree1, BASEDIR);
+
+	return ret;
+}
+
+ /*
+    basic testing of SMB2 change notify
+ */
+
+
 /*
    basic testing of SMB2 change notify
 */
@@ -2174,6 +2250,8 @@ struct torture_suite *torture_smb2_notify_init(void)
 				     torture_smb2_notify_rmdir3);
 	torture_suite_add_2smb2_test(suite, "rmdir4",
 				     torture_smb2_notify_rmdir4);
+	torture_suite_add_2smb2_test(suite, "close-after",
+				     torture_smb2_notify_close_after);
 
 	suite->description = talloc_strdup(suite, "SMB2-NOTIFY tests");
 
